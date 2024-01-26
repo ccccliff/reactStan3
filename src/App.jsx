1ba1d32753b0c3858496bd2782783e876671c875
ocean <twyeo9006@gmail.com>
2024-01-26 16:20:35 +0900

    update:first

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   const students = [
@@ -7,15 +7,22 @@ function App() {
     { name: "Charlie", age: 16, grade: "C" },
     { name: "Diana", age: 19, grade: "D" },
   ];
-
+const [adults,setAdults] = useState([])
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter(function(student){
+    return student.age >= 18;
+  });
+
+  const adultsBtn = function () {
+    
+  }
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
         {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
+        <button onClick={adultsBtn}></button>
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
       </ul>
     </div>
